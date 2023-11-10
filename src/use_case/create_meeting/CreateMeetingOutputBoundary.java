202fba921e60c7d3f5e6236bc2b0b35444a0c27d
Makoto Takahara <takaharian@gmail.com>
2023-11-10 18:36:00 -0500

    Created CreateMeetingOutputBoundary

--- src/use_case/create_meeting/CreateMeetingOutputBoundary.java
+++ src/use_case/create_meeting/CreateMeetingOutputBoundary.java
@@ -1,4 +1,7 @@
 package use_case.create_meeting;
 
 public class CreateMeetingOutputBoundary {
+    void prepareFailView(String error);
+
+    void prepareSuccessView(CreateMeetingOutputData createMeetingOutputData);
 }
