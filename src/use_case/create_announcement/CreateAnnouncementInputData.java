57c3ecb7d5057b6477285445ae41fb835fc583d5
Kosei1227 <koseiuemura1227@gmail.com>
2023-11-11 16:07:51 -0500

    Here, I added the missing parameters of author. Initially, I intended to add all the missing parameters and complement the functionality in debug branch, but it did not.

--- src/use_case/create_announcement/CreateAnnouncementInputData.java
+++ src/use_case/create_announcement/CreateAnnouncementInputData.java
@@ -6,14 +6,17 @@ public class CreateAnnouncementInputData {
 
     final private String message;
 
-    public CreateAnnouncementInputData(String announcementTitle, String message) {
+    final private String author;
+
+    public CreateAnnouncementInputData(String announcementTitle, String message, String author) {
         this.announcementTitle = announcementTitle;
         this.message = message;
+        this.author = author;
     }
 
     String getAnnouncementTitle(){return announcementTitle;}
 
     String getMessage(){return message;}
 
-
+    String getAuthor(){return author;}
 }
