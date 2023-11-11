497b8c3b2181fb0ee2f67b2da71c603462780aa5
ShioTatsu-Japan <tatsuya.shiokawa@mail.utoronto.ca>
2023-11-10 22:32:12 -0500

    I deleted the files I forgot to delete. I at first intended to make UpdateProject file but I noticed that this violates SOLID Principle, and thus made add_email and remove_email

--- src/use_case/add_project/UpdateProjectOutputBoundary.java
+++
@@ -1,6 +0,0 @@
-package use_case.update_project;
-
-public interface UpdateProjectOutputBoundary {
-    void prepareSuccessView(UpdateProjectOutputData project);
-    void prepareFailView(String error);
-}
