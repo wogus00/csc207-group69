61d7fad056107c19ab5bc94a4f757f8ada737033
Kosei1227 <koseiuemura1227@gmail.com>
2023-11-11 17:14:28 -0500

    Added data access methods in FirebaseAccessObject.java and made some changes relate to the data access.

--- src/use_case/delete_announcement/DeleteAnnouncementInputData.java
+++ src/use_case/delete_announcement/DeleteAnnouncementInputData.java
@@ -1,6 +1,26 @@
 package use_case.delete_announcement;
 
 public class DeleteAnnouncementInputData {
-    public DeleteAnnouncementInputData(){
+    private String announcementId;
+    private String userId;
+    public DeleteAnnouncementInputData(String announcementId, String userId){
+        this.announcementId = announcementId;
+        this.userId = userId;
+    }
+
+    public String getAnnouncementId() {
+        return announcementId;
+    }
+
+    public void setAnnouncementId(String announcementId) {
+        this.announcementId = announcementId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 }
