8e083afef015f8fae2078882cbbb0508835724f2
Jaehyeon Park <jpark0116@gmail.com>
2023-11-14 14:41:42 -0500

    added CreateTaskInputData class

---
+++ src/use_case/create_task/CreateTaskInputData.java
@@ -0,0 +1,49 @@
+package use_case.create_task;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CreateTaskInputData {
+    private final String projectName;
+    private final String taskName;
+    private final String supervisor;
+    private final String workingMembersList;
+    private final String deadline;
+    private final String comments;
+    private boolean status;
+
+    public CreateTaskInputData(String projectName, String taskName, String supervisor, String workingMembers, String deadline, String comments) {
+        this.projectName = projectName;
+        this.taskName = taskName;
+        this.supervisor = supervisor;
+        this.comments = comments;
+        this.workingMembersList = workingMembers;
+        this.deadline = deadline;
+        this.status = false;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+    public String getTaskName() {
+        return taskName;
+    }
+    public String getSupervisor() {
+        return supervisor;
+    }
+    public String getComments() {
+        return comments;
+    }
+    public String getDeadline() {
+        return deadline;
+    }
+    public String getWorkingMembersList() {
+        return workingMembersList;
+    }
+    public boolean getStatus() {
+        return status;
+    }
+}
