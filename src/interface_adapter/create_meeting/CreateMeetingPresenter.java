72398e27928314fd1d1e0e0abda9a205a4e56db3
Makoto Takahara <takaharian@gmail.com>
2023-11-20 12:22:11 -0500

    Fixing Bugs in CreateMeetingPresenter

--- src/interface_adapter/create_meeting/CreateMeetingPresenter.java
+++ src/interface_adapter/create_meeting/CreateMeetingPresenter.java
@@ -1,22 +1,38 @@
 package interface_adapter.create_meeting;
 
 import interface_adapter.ViewManagerModel;
+import interface_adapter.main_page.MainPageState;
+import interface_adapter.main_page.MainPageViewModel;
 import use_case.create_meeting.CreateMeetingOutputBoundary;
+import interface_adapter.create_meeting.CreateMeetingState;
 import use_case.create_meeting.CreateMeetingOutputData;
 
-public class CreateMeetingPresenter extends CreateMeetingOutputBoundary {
+public class CreateMeetingPresenter implements CreateMeetingOutputBoundary {
 
     private final CreateMeetingViewModel createMeetingViewModel;
+    private final MainPageViewModel mainPageViewModel;
+    private ViewManagerModel viewManagerModel;
 
     public CreateMeetingPresenter(ViewManagerModel viewManagerModel,
-                                  CreateMeetingViewModel createMeetingViewModel) {
+                                  CreateMeetingViewModel createMeetingViewModel,
+                                  MainPageViewModel mainPageViewModel) {
         this.viewManagerModel = viewManagerModel;
         this.createMeetingViewModel = createMeetingViewModel;
+        this.mainPageViewModel = mainPageViewModel;
     }
 
     @Override
-    public void prepareSuccessView(CreateProjectOutputData response) {
-        // On success, switch to the project dashboard view.
+    public void prepareSuccessView(CreateMeetingOutputData response) {
+        MainPageState mainPageState = new MainPageState();
+        mainPageState.setMeetingName(response.getMeetingName());
+        mainPageState.setParticipantEmail(response.getParticipantEmail());
+        mainPageState.setMeetingDate(response.getMeetingDate());
+        mainPageState.setStartTime(response.getStartTime());
+        mainPageState.setEndTime(response.getEndtime());
+        mainPageState.setProjectName(response.getProjectName());
+        mainPageViewModel.setState(mainPageState);
+        viewManagerModel.setActiveView(mainPageViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
     }
 
     @Override
