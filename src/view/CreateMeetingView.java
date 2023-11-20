9950f6edfe9030fa02b153667a6c608dee636123
Makoto Takahara <takaharian@gmail.com>
2023-11-20 12:22:35 -0500

    Fixing Bugs in CreateMeetingView

--- src/view/CreateMeetingView.java
+++ src/view/CreateMeetingView.java
@@ -15,15 +15,17 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.beans.PropertyChangeListener;
 
+
 public class CreateMeetingView extends JPanel implements ActionListener, PropertyChangeListener {
     public final String viewName = "create meeting";
 
     private final CreateMeetingViewModel createMeetingViewModel;
     private final JTextField meetingNameInputField = new JTextField(15);
     private final JTextField participantEmailInputField = new JTextField(15);
-    private final Scanner meetingDateInputField = new Scanner(System.in);
-    private final Time startTimeInputField = new Time(15);
-    private final Time endTimeInputField = new Time(15);
+    private final JTextField meetingDateInputField = new JTextField(15);
+    private final JTextField startTimeInputField = new JTextField(15);
+    private final JTextField endTimeInputField = new JTextField(15);
+    private final JTextField projectNameInputField = new JTextField(15);
     private final CreateMeetingController createMeetingController;
 
     private final JButton create;
@@ -49,6 +51,8 @@ public class CreateMeetingView extends JPanel implements ActionListener, Propert
                 new JLabel(CreateMeetingViewModel.START_TIME_LABEL), startTimeInputField);
         LabelTextPanel endTimeInfo = new LabelTextPanel(
                 new JLabel(CreateMeetingViewModel.END_TIME_LABEL), endTimeInputField);
+        LabelTextPanel projectNameInfo = new LabelTextPanel(
+                new JLabel(CreateMeetingViewModel.PROJECT_NAME_LABEL), projectNameInputField);
 
         JPanel buttons = new JPanel();
         create = new JButton(CreateMeetingViewModel.CREATE_BUTTON_LABEL);
@@ -66,7 +70,8 @@ public class CreateMeetingView extends JPanel implements ActionListener, Propert
                                     currentState.getParticipantEmail(),
                                     currentState.getMeetingDate(),
                                     currentState.getStartTime(),
-                                    currentState.getEndTime()
+                                    currentState.getEndTime(),
+                                    currentState.getProjectName()
                             );
                         }
                     }
@@ -187,7 +192,7 @@ public class CreateMeetingView extends JPanel implements ActionListener, Propert
         this.add(title);
         this.add(meetingNameInfo);
         this.add(participantEmailInfo);
-        this.add(meetingDatelInfo);
+        this.add(meetingDateInfo);
         this.add(startTimeInfo);
         this.add(endTimeInfo);
         this.add(buttons);
