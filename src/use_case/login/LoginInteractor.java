6b4802d79e0ecd4361047660314e8b7e9117a41e
sunshuh2 <shuhan.sun@mail.utoronto.ca>
2023-11-20 20:29:45 -0500

    fix error for an empty member email list

--- src/use_case/login/LoginInteractor.java
+++ src/use_case/login/LoginInteractor.java
@@ -16,12 +16,25 @@ public class LoginInteractor implements LoginInputBoundary {
 
     @Override
     public void execute(LoginInputData loginInputData) {
+        /**
+          Login in into a project with the user email. It sets up the success view in login presenter if
+          the project exists and the user email is a member in the project. It sets up the fail view with
+          error message either the project does not exist or the user email is not a member in the project.
+
+          @param loginInputData is the only parameter we have here, it contains information
+         *                       about the name of project we are logging into, and the user email
+         *                       for the user who is logging in.
+         */
+
         String projectName = loginInputData.getProjectName();
         String userEmail = loginInputData.getUserEmail();
         if (!userDataAccessObject.existsByName(projectName) ) {
             loginPresenter.prepareFailView("Project does not exist.");
         } else {
             ArrayList<String> memberEmails = userDataAccessObject.getProjectInfo(projectName).getMemberEmails();
+            if (memberEmails == null) {
+                memberEmails = new ArrayList<>();
+            }
             String leaderEmail = userDataAccessObject.getProjectInfo(projectName).getLeaderEmail();
             if (!userEmail.equals(leaderEmail) && !memberEmails.contains(userEmail)) {
                 loginPresenter.prepareFailView("User email " + userEmail + " does not exist for the project " + projectName + ".");
