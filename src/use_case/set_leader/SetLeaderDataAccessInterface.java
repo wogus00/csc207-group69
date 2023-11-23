eac44688d3e74132fd6155888c4931ad28e104ad
ShioTatsu-Japan <tatsuya.shiokawa@mail.utoronto.ca>
2023-11-23 18:42:37 -0500

    wrote java doc for use_case set_leader

--- src/use_case/set_leader/SetLeaderDataAccessInterface.java
+++ src/use_case/set_leader/SetLeaderDataAccessInterface.java
@@ -1,5 +1,19 @@
 package use_case.set_leader;
 
+/**
+ * The {@code SetLeaderDataAccessInterface} interface defines the contract for setting a new leader for a project.
+ * It is part of the data access layer, which interacts with the data source or database.
+ */
 public interface SetLeaderDataAccessInterface {
+
+    /**
+     * Updates the project data in the data source to set a new leader.
+     *
+     * This method is responsible for finding the project by its name in the data source and then updating
+     * its leader to the specified leader. This is a key part of managing project leadership within the system.
+     *
+     * @param projectName the name of the project for which the leader is to be set
+     * @param leader_name the name of the new leader to be set for the project
+     */
     void SetLeaderToNewLeader(String projectName, String leader_name);
 }
--- src/use_case/set_leader/SetLeaderOutputBoundary.java
+++ src/use_case/set_leader/SetLeaderOutputBoundary.java
@@ -1,6 +1,25 @@
 package use_case.set_leader;
 
+/**
+ * The {@code SetLeaderOutputBoundary} interface defines the contract for the output boundary
+ * of the set leader use case. It outlines methods for responding to the outcome of a request
+ * to set a new project leader, allowing for appropriate user feedback.
+ */
 public interface SetLeaderOutputBoundary {
+
+    /**
+     * Prepares the view to indicate the successful setting of a new project leader.
+     * This method should be invoked when the change of leadership has been completed
+     * successfully and the user interface needs to reflect this success.
+     */
     void prepareSuccessView();
+
+    /**
+     * Prepares the view to display an error message indicating that setting a new project leader
+     * has failed.
+     *
+     * @param error A string describing the error that occurred while attempting to set a new project leader.
+     *              This message should be user-friendly and possibly provide guidance on how to correct the issue.
+     */
     void prepareFailView(String error);
 }
