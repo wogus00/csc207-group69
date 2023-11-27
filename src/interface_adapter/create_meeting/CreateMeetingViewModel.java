945d7aa6a5cdc5ca2ecc148bbc33e701beded6bf
Makoto Takahara <takaharian@gmail.com>
2023-11-27 11:34:28 -0500

    Requiring emails in specific format as well

--- src/interface_adapter/create_meeting/CreateMeetingViewModel.java
+++ src/interface_adapter/create_meeting/CreateMeetingViewModel.java
@@ -14,7 +14,7 @@ public class CreateMeetingViewModel extends ViewModel {
     public static final String CANCEL_BUTTON_LABEL = "Cancel";
     public static final String TITLE_LABEL = "Create Meeting View";
     public static final String MEETING_NAME_LABEL = "Enter meeting name";
-    public static final String PARTICIPANT_EMAIL_LABEL = "Enter all participants' email";
+    public static final String PARTICIPANT_EMAIL_LABEL = "Enter all participants' emails separated by commas";
     public static final String MEETING_DATE_LABEL = "Enter meeting date (MM/DD/YYYY)";
     public static final String START_TIME_LABEL = "Enter start time (hh:mm in 24h format)";
     public static final String END_TIME_LABEL = "Enter end time (hh:mm in 24h format)";
